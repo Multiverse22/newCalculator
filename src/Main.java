8c2abf874718384b44900010e6f3d5e2a73d532e
multiverse22 <momizi527@gmail.com>
2024-07-24 11:55:28 +0900

    feat:level1 2번째 요구사항

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,9 @@ public class Main {
         System.out.println("두 번째 숫자를 입력하세요: ");
         int num2=sc.nextInt();
         System.out.println(num1+","+num2);
+        System.out.println("원하시는 사칙연산 기호를 입력하세요");
+        char basicOperator=sc.next().charAt(0);
+        System.out.println(basicOperator);
      }
 
 }
