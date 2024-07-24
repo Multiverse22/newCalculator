2b5d596a64f79018fb87008fe29faa58e4e5db67
multiverse22 <momizi527@gmail.com>
2024-07-24 11:54:14 +0900

    feat:level1 첫번째 요구사항

--- src/Main.java
+++ src/Main.java
@@ -11,42 +11,7 @@ public class Main {
         //String buffer=sc.nextLine();
         System.out.println("두 번째 숫자를 입력하세요: ");
         int num2=sc.nextInt();
-        //System.out.println(num1+","+num2);
-        System.out.println("원하시는 사칙연산 기호를 입력하세요");
-        char basicOperator=sc.next().charAt(0);
-        System.out.println(basicOperator);
-        
-        //System.out.println(basicOperator);
-        switch (basicOperator) {
-            case '/':
-                if (num2 == 0)
-                //어떤수를 0으로 나눌수는 없음
-                {
-                    System.out.println("나눗셈 연산에서 분모에 0이 들어갈 수 없습니다 error:1");
-                    break;
-                }
-                result = num1 / num2;
-                System.out.println("결과값은"+result+"입니다");
-                break;
-            case '*':
-                result = num1 * num2;
-                System.out.println("결과값은"+result+"입니다");
-                break;
-
-            case '+':
-                result = num1 + num2;
-                System.out.println("결과값은"+result+"입니다");
-                break;
-            case '-':
-                result = num1 - num2;
-                System.out.println("결과값은"+result+"입니다");
-                break;
-            default:
-                System.out.println("잘못된 사칙연산기호 " + basicOperator + "를 입력하셨습니다 + - * / 기호중에서 다시 시도하십시오");
-                break;
-        }
-
-
+        System.out.println(num1+","+num2);
      }
 
 }
